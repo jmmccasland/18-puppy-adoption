790f244b6a71a4780de35e258cc0a6986386802f
John Michael McCasland <jmmccasland@gmail.com>
2017-03-10 15:16:50 -0600

    nav bar

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,13 +1,14 @@
 <template lang="html">
   <div class="app">
-    <div class="section section--header">
-      <h1 class="is-brand logo"><router-link v-bind:to="{ name: 'index' }"> Puppies</router-link></h1>
-      <ul class="nav-list">
-        <li class="nav-list__item"><router-link>All Puppies</router-link></li>
-        <li class="nav-list__item"><router-link>Add Puppy</router-link></li>
-
-      </ul>
+    <div class="nav">
+      <div class="nav-left">
+        <router-link class="nav-item is-brand" v-bind:to="{ name: 'index' }">Puppies</router-link>
+      </div>
+    <div class="nav-right">
+      <router-link class="nav-item" v-bind:to="{ name: 'index' }">All Puppies</router-link>
+      <router-link class="nav-item" v-bind:to="{ name: 'index' }">Add Puppy</router-link>
     </div>
+  </div>
 
 
 

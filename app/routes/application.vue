0aeb70e217629765a7a21d9de546f07872c77b23
John Michael McCasland <jmmccasland@gmail.com>
2017-03-10 13:25:28 -0600

    Add router links to nav

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,5 +1,19 @@
 <template lang="html">
-  <div class="">
+  <div class="app">
+    <div class="section section--header">
+      <h1 class="is-brand logo"><router-link v-bind:to="{ name: 'index' }"> Puppies</router-link></h1>
+      <ul class="nav-list">
+        <li class="nav-list__item"><router-link>All Puppies</router-link></li>
+        <li class="nav-list__item"><router-link>Add Puppy</router-link></li>
+
+      </ul>
+    </div>
+
+
+
+
+
+
 
   </div>
 </template>
